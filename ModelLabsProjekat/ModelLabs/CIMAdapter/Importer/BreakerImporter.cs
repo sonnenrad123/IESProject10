48861b27585243d76bf4a8d1b594f815af0a6c4c
Ivan Gajic <33852380+sonnenrad123@users.noreply.github.com>
2021-02-28 21:07:18 +0100

    WrittenModelCodes

---
+++ ModelLabsProjekat/ModelLabs/CIMAdapter/Importer/BreakerImporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
+{
+    class BreakerImporter
+    {
+    }
+}
